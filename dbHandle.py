f870e2c0f8277a20ee77234a7b3eb2e278385c39
hexvaara <hexvaara@hex.local>
2019-04-08 11:00:32 +0300

    1, db initialization fixed

--- dbHandle.py
+++ dbHandle.py
@@ -5,7 +5,7 @@ conn_string = "host=localhost port=5432 dbname=pubgstatz user=pubgstatz password
 
 #conn = psycopg2.connect(conn_string)
 
-sql_command = "SELECT * FROM test"
+#sql_command = "SELECT * FROM test"
 
 #data = pd.read_sql(sql_command, conn)
 
@@ -22,7 +22,7 @@ def query(connection, queryString):
 
 def sqlCommand(connection, command):
     try:
-        getCursor(connection).execute(sql_command)
+        getCursor(connection).execute(command)
         connection.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error: ", error)
@@ -32,20 +32,20 @@ def initDB(connection):
     sqlCommand(connection, "CREATE TABLE IF NOT EXISTS matches (id SERIAL PRIMARY KEY, mode VARCHAR NOT NULL, map VARCHAR NOT NULL, json VARCHAR NOT NULL)")
     sqlCommand(connection, "CREATE TABLE IF NOT EXISTS telemetries (id SERIAL PRIMARY KEY, json VARCHAR NOT NULL)")
 
-def saveUser(connection, username, userJson):
-    try:
-        psql_insert_query = """ INSERT INTO users (id, json) VALUES (%s, %s)"""
-        record_to_insert = (username, userJson)
-        getCursor(connection).execute("INSERT INTO users ('kake','kakendatat')")
-    except (Exception, psycopg2.Error) as error:
-        print("Failed to save user: ", error)
+#def saveUser(connection, username, userJson):
+#    try:
+#        psql_insert_query = """ INSERT INTO users (id, json) VALUES (%s, %s)"""
+#        record_to_insert = (username, userJson)
+#        getCursor(connection).execute("INSERT INTO users (123,'kakendatat')")
+#    except (Exception, psycopg2.Error) as error:
+#        print("Failed to save user: ", error)
 
 
 conn = getDBConnection("localhost", "5432", "pubgstatz", "pubgstatz", "pubgstatz")
 
-data = query(conn, sql_command)
+#data = query(conn, sql_command)
 
-print(data)
+#print(data)
 
 initDB(conn)
-saveUser(conn, "kake", "kakendatat")
+# saveUser(conn, "kake", "kakendatat")
