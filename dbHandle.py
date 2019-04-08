25972c6de6a8ed8c9a0c0a73c5ba0f7b9a3dc400
hexvaara <hexvaara@hex.local>
2019-04-08 17:08:33 +0300

    1, save user working

--- dbHandle.py
+++ dbHandle.py
@@ -32,20 +32,23 @@ def initDB(connection):
     sqlCommand(connection, "CREATE TABLE IF NOT EXISTS matches (id SERIAL PRIMARY KEY, mode VARCHAR NOT NULL, map VARCHAR NOT NULL, json VARCHAR NOT NULL)")
     sqlCommand(connection, "CREATE TABLE IF NOT EXISTS telemetries (id SERIAL PRIMARY KEY, json VARCHAR NOT NULL)")
 
-#def saveUser(connection, username, userJson):
-#    try:
-#        psql_insert_query = """ INSERT INTO users (id, json) VALUES (%s, %s)"""
-#        record_to_insert = (username, userJson)
-#        getCursor(connection).execute("INSERT INTO users (123,'kakendatat')")
-#    except (Exception, psycopg2.Error) as error:
-#        print("Failed to save user: ", error)
+def saveUser(connection, userId, userJson):
+    try:
+        #psql_insert_query = """ INSERT INTO users (id, json) VALUES (%s, %s)"""
+        #record_to_insert = (userId, userJson)
+        getCursor(connection).execute("INSERT INTO users VALUES("+str(userId)+",'"+userJson+"')")
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to save user: ", error)
 
 
 conn = getDBConnection("localhost", "5432", "pubgstatz", "pubgstatz", "pubgstatz")
 
-#data = query(conn, sql_command)
-
-#print(data)
 
 initDB(conn)
-# saveUser(conn, "kake", "kakendatat")
+saveUser(conn, 234, "kakendatat")
+
+
+data = query(conn, "SELECT * FROM users")
+
+print(data)
